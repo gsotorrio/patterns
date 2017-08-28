0876345f95d14b71c3de00e40564392f647a7c1b
gsotorrio <willybosko@gmail.com>
2017-08-28 08:48:44 +0200

    New idea.

--- Cash machine/objectCash.js
+++ Cash machine/objectCash.js
@@ -3,20 +3,24 @@
 function Note(value) {
     var value = value;
 
-    this.countNotes = function(billetes){
-        var note = Math.floor(value / billetes);
+    this.countNotes = function(billete){
+        var note = Math.floor(value / billete);
 
         if(note > 0){
-            value = value - billetes;
-            console.log(billetes);
-            this.countNotes(500);
-        }
-        else{
-            this.countNotes()
+            value = value - billete;
+            console.log(billete);
         }
     }
-}
+} 
 
+Note.prototype = {
+    get: function(bill){
+        this.countNotes(bill)
+    }
+}   
 
+var myNote = new Note(885);
+
+myNote.get(500).get(200)
 
 [500, 200, 100, 50, 20, 10, 5]
