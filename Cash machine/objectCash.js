e9c14f11033a32f719b14b4ddf999544fd80b153
gsotorrio <willybosko@gmail.com>
2017-08-28 09:18:44 +0200

    Coppy solution

--- Cash machine/objectCash.js
+++ Cash machine/objectCash.js
@@ -9,18 +9,34 @@ function Note(value) {
         if(note > 0){
             value = value - billete;
             console.log(billete);
+            this.countNotes(billete)
+        }
+        else{
+
         }
     }
 } 
 
 Note.prototype = {
-    get: function(bill){
-        this.countNotes(bill)
-    }
+
 }   
 
-var myNote = new Note(885);
+//[500, 200, 100, 50, 20, 10, 5]
 
-myNote.get(500).get(200)
+var Request = function(value) {
+    this.value = value;
+    console.log("Show me the money " + value);
+}
 
-[500, 200, 100, 50, 20, 10, 5]
+Request.prototype = {
+    get: function(bill) {
+        var count = Math.floor(this.value / bill);
+        this.value -= count * bill;
+        console.log("bill " + bill)
+        return this;
+    }
+}
+
+var request = new Request(885);
+
+request.get(500).get(200).get(100).get(50).get(20).get(10).get(5)
